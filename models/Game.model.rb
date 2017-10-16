bdb59714b4d8ed03d38ffae4eeb7d3776c313d24
Chris Marshall <cmarshall10450@gmail.com>
2017-10-16 22:02:28 +0100

    Refactored game logic.

--- models/Game.model.rb
+++ models/Game.model.rb
@@ -3,19 +3,33 @@ class Game
 	def initialize(player_1, player_2)
 		@player_1 = player_1
 		@player_2 = player_2
+
+		@choices = ['rock', 'paper', 'scissors']
 	end
 
 	def winner
+		@choices.rotate!(@choices.find_index(@player_1))
+
 		if @player_1 == @player_2
 			return "It's a tie."
-		elsif @player_1 == 'rock' && @player_2 != 'paper'
-			return 'Player 1 wins.'
-		elsif @player_1 == 'scissors' && @player_2 != 'rock'
-			return 'Player 1 wins.'
-		elsif @player_1 == 'paper' && @player_2 != 'scissors'
-			return 'Player 1 wins.'
-		else
-			return 'Player 2 wins.'
 		end
+
+		if @choices.find_index(@player_2) == @choices.find_index(@player_1) + 1
+			return 'Player 2 wins.'
+		else
+			return 'Player 1 wins.'
+		end
+
+		# if @player_1 == @player_2
+		# 	return "It's a tie."
+		# elsif @player_1 == 'rock' && @player_2 != 'paper'
+		# 	return 'Player 1 wins.'
+		# elsif @player_1 == 'scissors' && @player_2 != 'rock'
+		# 	return 'Player 1 wins.'
+		# elsif @player_1 == 'paper' && @player_2 != 'scissors'
+		# 	return 'Player 1 wins.'
+		# else
+		# 	return 'Player 2 wins.'
+		# end
 	end
 end
